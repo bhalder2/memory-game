f57428d7e69432c91bd23e0f65249300b1191851
Brice Halder <bhalder2@jhu.edu>
2018-09-21 23:03:21 -0400

    implemented basic game functionality -> game is playable

--- src/main/java/memory/Board.java
+++ src/main/java/memory/Board.java
@@ -8,8 +8,10 @@ import java.util.ArrayList;
 import java.util.Collections;
 import java.util.HashSet;
 import java.util.List;
+import java.util.Objects;
 import java.util.Random;
 import java.util.Set;
+import java.util.regex.Pattern;
 
 /**
  *  Maintains the current state of the board using a hash table of
@@ -18,7 +20,7 @@ import java.util.Set;
  */
 class Board {
 
-    private class Card {
+    private static class Card {
 
         private String symbol;
 
@@ -29,6 +31,19 @@ class Board {
         @Override
         public String toString() {
             return symbol;
+        }
+
+        @Override
+        public boolean equals(Object o) {
+            if (this == o) return true;
+            if (o == null || getClass() != o.getClass()) return false;
+            Card card = (Card) o;
+            return Objects.equals(symbol, card.symbol);
+        }
+
+        @Override
+        public int hashCode() {
+            return Objects.hash(symbol);
         }
     }
 
@@ -42,22 +57,39 @@ class Board {
 
     private static int cols;
 
-    Board(int numRows, int numCols) {
-        rows = numRows;
-        cols = numCols;
+    // String declarations for coloring cards
+    static String RED = "\033[107;31m";
+    static String BLACK = "\033[107;30m";
+    static String RESET = "\033[49;39m";
+    static String RESET_BACK = "\033[0m";  // Text Reset
 
-        cards = new ArrayList<Card>(rows * cols);
-        unmatched = new HashSet<Card>(rows * cols);
+    static String REDTEXT = "\033[31;49m";
+
+    static String ANSI_WHITE_BACKGROUND = "\u001B[47m";
+    static String ANSI_RESET = "\u001B[0m";
+
+    static String ANSI_RED = "\u001B[31m";
+
+
+    static String blankCard = ANSI_RED;
+
+    static void setUp(int r, int c) {
+        rows = r;
+        cols = c;
+
+        cards = new ArrayList<>(rows * cols);
+        unmatched = new HashSet<>(rows * cols);
 
         for (int i = 0; i < (rows * cols) / 2; i++) {
             String symbol;
 
-            // Generate a random symbol, if the set already contains that
-            // card, generate a new one until it finds a symbol not
+            // Generate a random symbol. If the set already contains a card with
+            // that symbol, generate a new one & repeat until it finds a symbol not
             // in the set
             do {
                 symbol = randomEmoji();
             } while (unmatched.contains(new Card(symbol)));
+            assert (!unmatched.contains(new Card(symbol)));
 
             Card newCard = new Card(symbol);
 
@@ -77,6 +109,9 @@ class Board {
     }
 
     static boolean isMatch(String coord1, String coord2) {
+        if (coord1 == null || coord2 == null) {
+            return false;
+        }
         int index1 = coordToIndex(coord1);
         int index2 = coordToIndex(coord2);
 
@@ -91,28 +126,93 @@ class Board {
      *  @return true if both of the cards are yet to be matched
      */
     static boolean validCoordinates(String coord1, String coord2) {
+
+        String lastCol = Character.toString((char) ('A' + (cols - 1)));
+        String expected = "[A-" + lastCol + "][1-" + rows + "]";
+        if (!coord1.matches(expected) || !coord2.matches(expected)) {
+            return false;
+        }
+
+        if (coord1.equals(coord2)) {
+            return false;
+        }
+
         Card one = cards.get(coordToIndex(coord1));
         Card two = cards.get(coordToIndex(coord2));
 
         return unmatched.contains(one) && unmatched.contains(two);
     }
 
-    static void print() {
-        for (Card card : cards) {
-            System.out.print(card);
+    static void print(String coord1, String coord2) {
+        System.out.print("\n\t\t");
+        for (int i = 0; i < cols; i++) {
+            System.out.print((char) ('A' + i) + "\t");
         }
-        System.out.println();
-    }
 
-    private static String randomEmoji() {
-        return EmojiUtils.getEmoji("&#x1f42d;").getEmoji();
+        boolean match = Board.isMatch(coord1, coord2);
+
+        int index1 = coordToIndex(coord1);
+        int index2 = coordToIndex(coord2);
+
+        for (int i = 0; i < cards.size(); i++) {
+            if (i % cols == 0) {
+                System.out.print("\n" + RESET + "\n");
+                System.out.print(RESET + "\t" + ((i / cols) + 1) + "\t");
+            }
+
+            Card toPrint = cards.get(i);
+
+            if (i == index1 || i == index2) {
+                System.out.print(toPrint + "\t");
+                if (match) {
+                    unmatched.remove(toPrint);
+                }
+                continue;
+            }
+
+            if (unmatched.contains(toPrint)) {
+                System.out.print(RED + " " + RESET + "\t");
+            } else {
+                System.out.print(toPrint + "\t");
+            }
+        }
+        System.out.print("\n\n");
+
+        if (coord1 != null) {
+            System.out.println(match ? "Match!" : "No match!");
+
+            try {
+                Thread.sleep(2000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+
+            System.out.println("\033[H\033[2J");
+            Board.print(null, null);
+        }
     }
 
     private static int coordToIndex(String coord) {
-        int letter = coord.charAt(0) - 'A';
-        int num = coord.charAt(1) - '0';
+        if (coord == null) {
+            return -1;
+        }
+        String coordUpper = coord.toUpperCase();
+        int letter = coordUpper.charAt(0) - 'A';
+        int num = coordUpper.charAt(1) - '0';
 
         return letter + cols * (num - 1);
+    }
 
+    private static String randomEmoji() {
+        Emoji emoji;
+        do {
+            // Choose from 100 different emojis
+            String randEmojiDecimalCode = "&#" + (127789 + new Random().nextInt(100)) + ";";
+
+            // Returns the emoji corresponding to the given HTML Entity decimal code
+            // @source https://github.com/kcthota/emoji4j
+            emoji = EmojiUtils.getEmoji(randEmojiDecimalCode);
+        } while (emoji == null);
+        return emoji.getEmoji();
     }
 }
--- src/main/java/memory/Memory.java
+++ src/main/java/memory/Memory.java
@@ -1,7 +1,9 @@
 package memory;
 
 import java.util.ArrayList;
+import java.util.InputMismatchException;
 import java.util.List;
+import java.util.NoSuchElementException;
 import java.util.PriorityQueue;
 import java.util.Scanner;
 import java.util.regex.Pattern;
@@ -18,35 +20,61 @@ public class Memory {
      */
     public static void main(String[] args) {
 
-        System.out.print("Enter number of players: ");
+        int numPlayers = 0;
 
-        Scanner in = new Scanner(System.in);
-        int numPlayers = in.nextInt();
+        do {
+            Scanner in = new Scanner(System.in);
+            try {
+                System.out.print("\nEnter number of players: ");
+                numPlayers = in.nextInt();
+            } catch (InputMismatchException e) {
+                in.nextLine();
+            }
+        } while (numPlayers <= 0);
 
-        List<Player> players = new ArrayList<Player>(numPlayers);
+        List<Player> players = new ArrayList<>(numPlayers);
         for (int i = 0; i < numPlayers; i++) {
             players.add(new Player(i + 1));
         }
 
-        // Implement difficulties here
-        Board board = new Board(6, 6);
+        int rows = 0;
+        int cols = 0;
+        do {
+            Scanner in = new Scanner(System.in);
+            try {
+                System.out.print("\nEnter rows and columns (e.g. \"2 3\" for a 2x3 board): ");
+                rows = in.nextInt();
+                cols = in.nextInt();
+            } catch (InputMismatchException e) {
+                in.nextLine();
+            }
+        } while (rows <= 0 || cols <= 0);
 
-        // Strings to store user-inputted coordinates of cards
-        String coord1, coord2;
+        // Implement difficulties here
+        Board.setUp(rows, cols);
+
+        // Print board with all cards face down
+        Board.print(null, null);
 
         // Loop as long as the board has unmatched card pairings still in play
         while (Board.hasUnmatched()) {
             for (Player player : players) {
-                System.out.println("Player " + player.getPlayerNumber() + "!");
+                System.out.println("\nPlayer " + player.getPlayerNumber() + "!");
+
+                // Strings to store user-inputted coordinates of cards
+                String coord1 = null;
+                String coord2 = null;
 
                 // Repeat until user enters valid coordinates of two unmatched cards
                 do {
+                    Scanner in = new Scanner(System.in);
                     System.out.print("Enter two coordinates: ");
-                    coord1 = in.next(Pattern.compile("[A-F][1-6]"));
-                    coord2 = in.next(Pattern.compile("[A-F][1-6]"));
+
+                    coord1 = in.next().toUpperCase();
+                    coord2 = in.next().toUpperCase();
                 } while (!Board.validCoordinates(coord1, coord2));
 
-                Board.print();
+                Board.print(coord1, coord2);
 
                 // If the cards are a match, increment the player's score
                 // else, increment their # of non-matching pairs chosen
@@ -55,21 +83,23 @@ public class Memory {
                 } else {
                     player.increaseNonmatches();
                 }
+
+                if (!Board.hasUnmatched()) {
+                    break;
+                }
             }
         }
 
         // Build a max-heap of the players' scores
-        PriorityQueue<Player> scores = new PriorityQueue<Player>(players);
+        PriorityQueue<Player> scores = new PriorityQueue<>(players);
 
         // Print each of the players' scores in descending order
-        int count = 1;
         System.out.println("SCORES\n------------\n");
+
         while (!scores.isEmpty()) {
             Player p = scores.poll();
-            System.out.println("Player " + p.getPlayerNumber() + "\t" + p.getScore());
-            count++;
+            System.out.println("Player " + p.getPlayerNumber() + "\t\tMatches:\t" + p.getScore() +
+                    "\t\tNon-matches:\t" + p.getNonMatches());
         }
-
-
     }
 }
