6cd50d8f0cb42e183fb53f2c353e94170c0cf12b
Brice Halder <bhalder2@jhu.edu>
2018-09-21 23:04:58 -0400

    Revert "implemented basic game functionality -> game is playable"
    This reverts commit f57428d7e69432c91bd23e0f65249300b1191851.

--- src/main/java/memory/Memory.java
+++ src/main/java/memory/Memory.java
@@ -1,9 +1,7 @@
 package memory;
 
 import java.util.ArrayList;
-import java.util.InputMismatchException;
 import java.util.List;
-import java.util.NoSuchElementException;
 import java.util.PriorityQueue;
 import java.util.Scanner;
 import java.util.regex.Pattern;
@@ -20,61 +18,35 @@ public class Memory {
      */
     public static void main(String[] args) {
 
-        int numPlayers = 0;
+        System.out.print("Enter number of players: ");
 
-        do {
-            Scanner in = new Scanner(System.in);
-            try {
-                System.out.print("\nEnter number of players: ");
-                numPlayers = in.nextInt();
-            } catch (InputMismatchException e) {
-                in.nextLine();
-            }
-        } while (numPlayers <= 0);
+        Scanner in = new Scanner(System.in);
+        int numPlayers = in.nextInt();
 
-        List<Player> players = new ArrayList<>(numPlayers);
+        List<Player> players = new ArrayList<Player>(numPlayers);
         for (int i = 0; i < numPlayers; i++) {
             players.add(new Player(i + 1));
         }
 
-        int rows = 0;
-        int cols = 0;
-        do {
-            Scanner in = new Scanner(System.in);
-            try {
-                System.out.print("\nEnter rows and columns (e.g. \"2 3\" for a 2x3 board): ");
-                rows = in.nextInt();
-                cols = in.nextInt();
-            } catch (InputMismatchException e) {
-                in.nextLine();
-            }
-        } while (rows <= 0 || cols <= 0);
-
         // Implement difficulties here
-        Board.setUp(rows, cols);
+        Board board = new Board(6, 6);
 
-        // Print board with all cards face down
-        Board.print(null, null);
+        // Strings to store user-inputted coordinates of cards
+        String coord1, coord2;
 
         // Loop as long as the board has unmatched card pairings still in play
         while (Board.hasUnmatched()) {
             for (Player player : players) {
-                System.out.println("\nPlayer " + player.getPlayerNumber() + "!");
-
-                // Strings to store user-inputted coordinates of cards
-                String coord1 = null;
-                String coord2 = null;
+                System.out.println("Player " + player.getPlayerNumber() + "!");
 
                 // Repeat until user enters valid coordinates of two unmatched cards
                 do {
-                    Scanner in = new Scanner(System.in);
                     System.out.print("Enter two coordinates: ");
-
-                    coord1 = in.next().toUpperCase();
-                    coord2 = in.next().toUpperCase();
+                    coord1 = in.next(Pattern.compile("[A-F][1-6]"));
+                    coord2 = in.next(Pattern.compile("[A-F][1-6]"));
                 } while (!Board.validCoordinates(coord1, coord2));
 
-                Board.print(coord1, coord2);
+                Board.print();
 
                 // If the cards are a match, increment the player's score
                 // else, increment their # of non-matching pairs chosen
@@ -83,23 +55,21 @@ public class Memory {
                 } else {
                     player.increaseNonmatches();
                 }
-
-                if (!Board.hasUnmatched()) {
-                    break;
-                }
             }
         }
 
         // Build a max-heap of the players' scores
-        PriorityQueue<Player> scores = new PriorityQueue<>(players);
+        PriorityQueue<Player> scores = new PriorityQueue<Player>(players);
 
         // Print each of the players' scores in descending order
+        int count = 1;
         System.out.println("SCORES\n------------\n");
-
         while (!scores.isEmpty()) {
             Player p = scores.poll();
-            System.out.println("Player " + p.getPlayerNumber() + "\t\tMatches:\t" + p.getScore() +
-                    "\t\tNon-matches:\t" + p.getNonMatches());
+            System.out.println("Player " + p.getPlayerNumber() + "\t" + p.getScore());
+            count++;
         }
+
+
     }
 }
